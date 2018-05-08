957823110f93bdd7bfe1e238ec1162643165ca21
snavdeepsingh <snavdeepsingh@gmail.com>
2018-05-08 00:35:44 -0500

    added api-routes and finished sequelize

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,16 +8,40 @@ module.exports = function(app){
 
 
     app.get("/", function(req,res){
-        res.send("Its working!");
+        // res.send("Its working!");
+        db.Burger.findAll({}).then(function(data){
+            var hbsObject = {
+                burgers: data
+            };
+
+            console.log(hbsObject);
+            res.render("index", hbsObject);
+            console.log("we are in burger");
+        })
+
     });
 
 
     app.post("/burgers", function(req,res){
+        db.Burger.create({
+            burger_name: req.body.burger_name
+        }).then(function(data){
+            res.json({ id: res.insertId});
+        })
 
     });
 
 
     app.put("/burgers/:id", function(req,res){
+        db.Burger.update({
+            devoured: req.body.devoured
+        },{
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data){
+            res.json(data);
+        })
 
     });
 };
